cf09e7457062f7e8682d6e769378d3073993ae8a
Anton S <residentsummer@gmail.com>
2020-09-15 12:28:30 +0300

    Allow installation for python 3.8

--- setup.py
+++ setup.py
@@ -28,8 +28,9 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    python_requires='>=2.7, <3.8',
+    python_requires='>=2.7, <3.9',
     install_requires=[
         'requests>=2.4',
     ],
